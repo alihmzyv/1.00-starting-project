09236dfb8a4a1019dc598b00cf74cb0fbca77bf6
alihmzyv <alihmzyv@gmail.com>
2023-01-04 20:57:54 +0400

    refactored code (tdd), still passes the tests

--- src/main/java/com/luv2code/junitdemo/tdd/FizzBuzz.java
+++ src/main/java/com/luv2code/junitdemo/tdd/FizzBuzz.java
@@ -1,10 +1,25 @@
 package com.luv2code.junitdemo.tdd;
 
 public class FizzBuzz {
-    private final static String fizzBuzz = "FizzBuzz";
     private final static String fizz = "Fizz";
     private final static String buzz = "Buzz";
+    private final static String fizzBuzz = "FizzBuzz";
+
     public static String compute(int num) {
+        StringBuilder result = new StringBuilder();
+        if (num % 3 == 0) {
+            result.append(fizz);
+        }
+        if (num % 5 == 0) {
+            result.append(buzz);
+        }
+        if (result.isEmpty()) {
+            result.append(num);
+        }
+        return result.toString();
+    }
+    
+    private static String compute2(int num) {
         if (num % 3 == 0) {
             if (num % 5 == 0) return fizzBuzz;
             return fizz;
