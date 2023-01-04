97185eccc7beb5100044542942c9c914e68a0088
alihmzyv <alihmzyv@gmail.com>
2023-01-04 20:44:45 +0400

    wrote code so that the tests can pass

--- src/main/java/com/luv2code/junitdemo/tdd/FizzBuzz.java
+++ src/main/java/com/luv2code/junitdemo/tdd/FizzBuzz.java
@@ -5,6 +5,9 @@ public class FizzBuzz {
         if (num % 3 == 0) {
             return "Fizz";
         }
+        else if (num % 5 == 0) {
+            return "Buzz";
+        }
         return null;
     }
 }
